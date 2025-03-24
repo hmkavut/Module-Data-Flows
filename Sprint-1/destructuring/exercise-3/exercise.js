97533e3cca755f378e9ec7604738fdadece1dcee
Aida Zhukova <aidazhukovaa@gmail.com>
2025-03-24 13:53:13 +0000

    Print Receipt exercise done

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,27 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printReceipt(order) {
+
+  const col1Width = 8;
+  const col2Width = 20;
+  console.log('QTY'.padEnd(col1Width) + 'ITEM'.padEnd(col2Width) + 'TOTAL')
+
+  let totalSum = 0;
+
+  for (const item of order) {
+    let { itemName, quantity, unitPricePence } = item;
+    let totalItemSum = unitPricePence * quantity
+    totalSum = totalItemSum + totalSum;
+    let unitPricePounds = (totalItemSum / 100).toFixed(2)
+    console.log(`${quantity.toString().padEnd(col1Width)}${itemName.padEnd(col2Width)}${unitPricePounds}`)
+  }
+
+  let totalPounds = (totalSum / 100).toFixed(2)
+  console.log('')
+  console.log(`Total: ${totalPounds.toString()}`);
+};
+
+printReceipt(order)
+
